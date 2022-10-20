3cf6d13d00e5c28b449473dbb969caf7ed86d6b8
Gauch0 <matiaslorenzo466@gmail.com>
2022-10-20 17:19:22 -0300

    add more outputs

--- variable.tf
+++ variable.tf
@@ -1,10 +1,28 @@
-#general
+#AWS REGION
 variable "region" {}
 
-#ec2
+#AWS EC2 Instance Name - Ami - Type
 variable "instance_type" {}
 variable "ami" {}
 variable "name" {}
 
-#Keypair
+#AWS EC2 Instance Key Pair
 variable "instance_keypair" {}
+
+#AWS EC2 Instance Type - List
+variable "instance_type_list" {
+  # description = "EC2 Instance Type List"
+  # type = list(string)
+  # default = ["t2.micro","t2.small"]
+}
+
+#AWS EC2 Instance Type - Map
+variable "instance_type_map" {
+  # description = "EC2 Instance Type"
+  # type = map(string)
+  # default = {
+  #   "test" = "t2.micro"
+  #   "qa"  =  "t3.small"
+  #   "prod" = "t3.large"
+  # }
+}
