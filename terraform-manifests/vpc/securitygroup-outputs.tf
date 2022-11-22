3bd6200a783183d0565f74fd831c91b7e0f03135
Gauch0 <matiaslorenzo466@gmail.com>
2022-11-22 10:00:32 -0300

    add vpc

--- terraform-manifests/vpc/securitygroup-outputs.tf
+++ terraform-manifests/vpc/securitygroup-outputs.tf
@@ -1,16 +1,35 @@
 
-
+## public_bastion_sg_group_id
 output "public_bastion_sg_group_id" {
-    description     = "The ID of the security group"
-    value           = module.complete_sg.this_security_group_id 
+    description         = "The ID of the security group"
+    value               = module.public_bastion_sg.this_security_group_id 
 }
 
+## public_bastion_sg_group_vpc_id
 output "public_bastion_sg_group_vpc_id" {
-    description     = "The VPC ID"
-    value           = modue.complete_sg.this_security_group_vpc_id 
+    description         = "The VPC ID"
+    value               = modue.public_bastion_sg.this_security_group_vpc_id 
 }
 
 output "public_bastion_sg_group_name" {
-    description     = "The name of the security"
-    value           = module.complete_sg.this_security_group_name
+    description         = "The Security Group Name ID"
+    value               = module.public_bastion_sg.this_security_group_name
+}
+
+# Private EC2 Instances Security Group Outputs
+## private_sg_group_id
+output "private_sg_group_id" {
+    description         = "The name of the security"
+    value               = module.private_bastion_sg.this_security_group_id
+}
+## private_sg_group_vpc_id
+output "private_sg_group_vpc_id" {
+    description         = "The name of the security"
+    value               = module.private_bastion_sg.this_security_group_vpc_id
+}
+
+## private_sg_group_name
+output "private_sg_group_name" {
+    description         = "The name of the security"
+    value               = module.private_bastion_sg.this_security_group_name
 }
